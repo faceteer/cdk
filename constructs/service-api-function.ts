37f9303edf1398bf0f5a31bdad8e09a21d2b5305
Sam Fletcher <sfletchertx@gmail.com>
2022-05-18 11:23:17 -0500

    Remove log group on destroy

--- constructs/service-api-function.ts
+++ constructs/service-api-function.ts
@@ -6,6 +6,7 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import type { HandlerNameAndPath } from '../extract/extract-handlers';
 import type { ApiHandlerDefinition } from '../handlers/api-handler';
+import { RemovalPolicy } from 'aws-cdk-lib';
 
 export interface ServiceApiFunctionProps {
 	role: iam.IRole;
@@ -69,6 +70,8 @@ export class ServiceApiFunction extends Construct {
 			},
 			layers,
 		});
+
+		this.fn.logGroup.applyRemovalPolicy(RemovalPolicy.DESTROY);
 
 		this.fn.grantInvoke(apiGatewayServicePrincipal);
 
